90dac76e8f6d035cb070f52ab6f82f4817f66be6
KA-DEVL <anes1999@live.ca>
2023-08-30 18:44:13 -0700

    Simple kafka consumer setup

---
+++ src/main/java/com/example/kafkaexample/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.example.kafkaexample.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+    @KafkaListener(topics = "sample-topic", groupId="myGroup")
+    public void consume(String message){
+        LOGGER.info("Message received! : " + message);
+    }
+}
